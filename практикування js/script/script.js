a987f0322ce45b619234989d4e45c64ba39bf0ea
aduvancik <2103kokakola2004@gmaik.com>
2023-03-19 18:36:54 +0200

    add practic js

---
+++ практикування js/script/script.js
@@ -0,0 +1,112 @@
+let block = document.querySelector(".block1");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const options = {
+//   "capture": false,
+//   "once": true,
+//   "passive": false
+// }
+// function log(){
+//   console.log('alla');
+//   // block.removeEventListener('click', log);
+// }
+// block.addEventListener('click', log, options);
+
+
+
+
+
+// function log(){
+//   console.log('alla');
+// }
+// block.addEventListener('click', log);
+
+
+
+// block.addEventListener("click", function(e) {
+//   console.log('Клик');
+// });
+// block.addEventListener("click", function() {
+//   console.log('Клfк');
+// })
+
+
+
+
+// const list = document.querySelector('ul');
+// list.insertAdjacentHTML(
+// 	'beforeend',
+// 	'<li>Текст</li>'
+// );
+// const body = document.querySelector('.a');
+// // 
+// const style = (getComputedStyle(body, "::before"));
+// const nubme = parseInt(style.fontSize);
+// console.dir(body);
+// console.log(body.tagName);
+// body.setAttribute('href', 'https://www.youtube.com/watch?v=DuWyc76lYC4&list=PLM6XATa8CAG4F9nAIYNS5oAiPotxwLFIr&index=51')
+
+// body.style.cssText = `color:red;
+// // 							margin:100px;`;
+// for (let i = 0; i < body.length; i++) {
+// 	body[i].addEventListener('mousedown', () => {
+// 		body[i].style.color='red';
+// 		});
+// }
+// for (let i = 0; i < body.length; i++) {
+// 	body[i].addEventListener('mouseup', () => {
+// 		body[i].style.color='green';
+// 		});
+// }
+// for (let i = 0; i < body.length; i++) {
+// 	body[i].addEventListener('mouseleave', () => {
+// 		body[i].style.color='black';
+// 		});
+// }
+// for (let i = 0; i < body.length; i++) {
+// 	body[i].addEventListener('mouseenter', () => {
+// 		body[i].style.color='white';
+// 		});
+// }
+// const style = getComputedStyle(body[0]);
+// console.log(style.fontFamily);
+// const textHeader = body.innerHTML = 
+// `<header><ul>
+// <li>Hello</li>
+// <li>my word</li>
+// </ul></header>`;
+// const ul = body.firstChild.firstChild;
+// console.log(ul.textContent);
+// ul.textContent = ` ${ul.textContent} text`;
+// body.insertAdjacentElement('afterbegin',ul)
+
+// ul.insertAdjacentHTML(
+// `afterend`,
+// ` <p class="clas">laal<span>elem</span></p>`
+// );
+// console.log(text);
+// text.innerHTML = 
+// `<p>${textContent}</p><p>я жиивий <span class="yellow">lala</span></p>`;
+// const body = document.head.firstElementChild;
+// const body = document.head.parentElement;
+// const body = document.body.previousElementSibling;
+// const body = document.head.nextElementSibling;
+// const text = document.querySelector('.text');
+// const textContent = text.innerHTML ;
+// console.log(text.textContent);
+// text.innerHTML = 
+// `<p>${textContent}</p><p>я жиивий <span class="yellow">lala</span></p>`;
+
