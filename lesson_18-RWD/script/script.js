661449cd3df9b53046ced859eb5682ef9dc5af90
aduvancik <2103kokakola2004@gmaik.com>
2023-04-18 19:35:54 +0300

    problem responsive

--- lesson_18-RWD/script/script.js
+++ lesson_18-RWD/script/script.js
@@ -1,1 +1,11 @@
-const list = document.querySelector('.list');
+const p = 3.14;
+
+let name = prompt('як тебе звати,Вова?', 'імя');
+let age = prompt('скільки тобі років?', '18');
+let line = 4 * parseInt(prompt('Введи сторону квадрату в см', '1см'));
+let radius = p * (parseInt(prompt('Введи радіус кола в см', '2см')) ** 2);
+
+alert(`Тебе звати ${name},тобі ${age} років!Сторона квадрату ${line}см, радіус кола:${radius} см`);
+
+let time = prompt('ку,за скільки часу ти плануєш до Москви дойти?', '1 год');
+
