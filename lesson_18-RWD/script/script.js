49e4563a460952b9ff5756162c803547d3b972cf
aduvancik <2103kokakola2004@gmaik.com>
2023-04-20 02:27:00 +0300

    add full responsiv

--- lesson_18-RWD/script/script.js
+++ lesson_18-RWD/script/script.js
@@ -1,11 +1,26 @@
 const p = 3.14;
 
-let name = prompt('як тебе звати,Вова?', 'імя');
-let age = prompt('скільки тобі років?', '18');
-let line = 4 * parseInt(prompt('Введи сторону квадрату в см', '1см'));
-let radius = p * (parseInt(prompt('Введи радіус кола в см', '2см')) ** 2);
+// const name = prompt('як тебе звати,Вова?', 'імя');
+// const age = prompt('скільки тобі років?', '18');
+// const line = 4 * parseInt(prompt('Введи сторону квадрату в см', '1см'));
+// const radius = p * (parseInt(prompt('Введи радіус кола в см', '2см')) ** 2);
+// alert(`Тебе звати ${name},тобі ${age} років!Сторона квадрату ${line}см, радіус кола:${radius} см`);
 
-alert(`Тебе звати ${name},тобі ${age} років!Сторона квадрату ${line}см, радіус кола:${radius} см`);
+// const time = parseInt(prompt('ку,за скільки часу ти плануєш до Москви дойти?', '1 год'));
+// const distance = parseInt(prompt('яка відстань', '1000км'));
+// alert((time / distance) + 'км / год' );
 
-let time = prompt('ку,за скільки часу ти плануєш до Москви дойти?', '1 год');
+const burgerMenu = document.querySelector('.header__burger');
+const listHeader = document.querySelector('.header__list');
+
+  burgerMenu.addEventListener("click", function() {
+    burgerMenu.classList.toggle("header__burger-click");
+    listHeader.classList.toggle('header__list-click');
+    
+    
+  });
+
+
+
+
 
