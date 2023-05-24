2a70ca62c39e3e8235314827b3561b174df6592c
aduvancik <2103kokakola2004@gmaik.com>
2023-05-24 23:42:47 +0300

    add responsive in products and partners

--- lesson-37-library/script/index.js
+++ lesson-37-library/script/index.js
@@ -1,16 +1,10 @@
-// const screenWidth = window.innerWidth;
-// let amountPartnersSwipe = 9;
-// if(screenWidth < 1300) {
-//   amountPartnersSwipe = 3;
-// } 
-
-window.scrollTo({
+window.scrollTo({ //перезагрузка сторінки
   top: 0,
   behavior: 'smooth'
 });
 document.body.style.overflow = 'hidden';
 
-new Swiper(".image-slider", {
+new Swiper(".image-slider", { //перший слайдер
 pagination: {
   el: ".swiper-pagination",
   clickable: true,
@@ -27,50 +21,54 @@ fadeEffect: {
 },
 });
 
-new Swiper(".products", {
+new Swiper(".products", { //другий слайдер
+  slidesPerGroup: 4,
+  pagination: {
+    el: ".products-pagination",
+    clickable: true,
+  },
   speed: 600,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev"
   },
   slidesPerView: 5,
-  spaceBetween: 50,
+  spaceBetween: 40,
   loop: true,
   loopedSlides: 3,
   initialSlides: 10,
   centeredSlides: true,
   breakpoints: {
-    // 960: {
-    //   slidesPerView: 4.6,
-    //   spaceBetween: 20
-    // },
-    // 800: {
-    //   slidesPerView: 3.2,
-    //   spaceBetween: 70
-    // },
-    // 780: {
-    //   slidesPerView: 4,
-    //   spaceBetween: 10
-    // },
-    // 540: {
-    //   slidesPerView: 2.6,
-    //   spaceBetween: 90
-    // },
-    // 320: {
-    //   slidesPerView: 1,
-    //   spaceBetween: 20
-    // },
+    1000: {
+      slidesPerView: 5,
+      spaceBetween: 40
+    },
+    700: {
+      slidesPerView: 3.2,
+      spaceBetween: 70
+    },
+    540: {
+      slidesPerView: 2.5,
+      spaceBetween: 20
+    },
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
   }
   });
 
-  new Swiper(".partners", {
+  new Swiper(".partners", { // третій слайдер
+    
+    pagination: {
+      el: ".partners-pagination",
+      clickable: true,
+    },
     speed: 300,
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev"
     },
-    // slidesPerView: 9, 
-    // spaceBetween: 50,
     loop: true,
     breakpoints: {
       960: {
@@ -87,7 +85,8 @@ new Swiper(".products", {
       },
       320: {
         slidesPerView: 2,
-        spaceBetween: 70
+        spaceBetween: 70,
+        slidesPerGroup: 6,
       },
     }
     });
@@ -98,7 +97,7 @@ function hidePreloader() {
   document.body.style.overflow = 'auto';
 }
 
-window.addEventListener("load", function() {
+window.addEventListener("load", function() {//сховати preloader
   setTimeout(hidePreloader, 1000);
 });
 
@@ -106,7 +105,7 @@ const scrollLinks = document.querySelectorAll(".header__list li")[3];
 const footer = document.querySelector(".footer")
 console.log(scrollLinks);
 
-scrollLinks.addEventListener('click', function(e) {
+scrollLinks.addEventListener('click', function(e) { //клік на contacts
     e.preventDefault();
     footer.scrollIntoView({
       behavior: 'smooth',
@@ -117,18 +116,26 @@ scrollLinks.addEventListener('click', function(e) {
 
 const statusElements = document.querySelectorAll('.products__status');
 
-statusElements.forEach(function(element) {
+statusElements.forEach(function(element) { //другий слайдер
   element.addEventListener('click', function(event) {
     if (event.target.tagName === 'A') {
       event.preventDefault(); 
       const link = event.target;
       link.textContent = 'sold';
+      link.style.color = '#000';
     }
 
     this.style.backgroundColor = '#F8F1EA';
   });
 });
 
+const burgerMenu = document.querySelector(".header__burger");
+const headerList = document.querySelector(".header__list");
+
+burgerMenu.addEventListener('click', function() { //бургер меню
+  headerList.classList.toggle("header__list-active");
+  burgerMenu.classList.toggle("header__burger-active");
+})
 
 
 
